ceeb98c63633fd190ddb5be41b7949e86acb1d82
xj <xj@example.com>
2018-07-26 20:17:16 +0800

    首页完成

--- src/main.js
+++ src/main.js
@@ -4,14 +4,32 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 // 引入index
 import index from './components/index.vue'
+// 引入ui模块
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// 引入lazyload
+import VueLazyload from 'vue-lazyload';
+const path = require('path');
+// 使用懒加载中间件
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  loading: path.join(__dirname, 'src/assets/img/01.gif')
+});
 // 使用路由中间件
 Vue.use(VueRouter)
+// 使用ul中间件
+Vue.use(ElementUI);
 // 注册路由规则
 const router = new VueRouter({
   routes: [{
-    path: '/index',
-    component: index
-  }]
+      path: '/',
+      redirect: '/index'
+    },
+    {
+      path: '/index',
+      component: index
+    }
+  ]
 })
 // 引入css
 import './assets/statics/site/css/style.css';
